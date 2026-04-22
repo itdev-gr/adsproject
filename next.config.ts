d3aea5424629a104b9546328b106425d1cc0f575
Marios <info@itdev.gr>
2026-04-22 22:32:00 +0300

    feat: wire up Sentry SDK (no-op until SENTRY_DSN is set)

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import './src/lib/env'
 import type { NextConfig } from 'next'
+import { withSentryConfig } from '@sentry/nextjs'
 
 const nextConfig: NextConfig = {
   /* config options here */
 }
 
-export default nextConfig
+export default process.env.SENTRY_AUTH_TOKEN
+  ? withSentryConfig(nextConfig, {
+      silent: !process.env.CI,
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      disableLogger: true,
+    })
+  : nextConfig
